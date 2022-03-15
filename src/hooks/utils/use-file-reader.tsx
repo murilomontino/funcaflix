9805f2bd4a19d0c55b644e347ff47c497b5890ea
murilomontino <murilomontinojr@hotmail.com>
2022-03-15 12:50:04 -0300

    [hooks] criação do hooks de leitura de arquivos

--- src/hooks/utils/use-file-reader.tsx
+++ src/hooks/utils/use-file-reader.tsx
@@ -1,29 +1,60 @@
 import { useCallback } from 'react'
 
+type Document = {
+  type: 'success' | 'error'
+  uri: string
+  name: string
+  size: number
+  mimeType: string
+}
+
+const EventFileReader = async (file: File): Promise<Document> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      resolve({
+        type: 'success',
+        uri: reader.result as string,
+        name: file.name,
+        size: file.size,
+        mimeType: file.type,
+      })
+    }
+
+    reader.onerror = () => {
+      return resolve({
+        type: 'error',
+        uri: '',
+        name: '',
+        size: 0,
+        mimeType: '',
+      })
+    }
+
+    reader.readAsDataURL(file)
+  })
+}
+
 const useFileReader = () => {
-  const fileReader = useCallback((fileList) => {
-    return Promise.all(
-      Object.keys(fileList).map(
-        (_key, i) =>
-          new Promise((resolve) => {
-            const reader = new FileReader()
-            const file = fileList[i]
+  const fileReader = useCallback(
+    async (fileList: File[] | File): Promise<Document[] | Document> => {
+      if (!Array.isArray(fileList)) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (await EventFileReader(fileList)) as any
+      }
 
-            reader.onload = () => {
-              resolve({
-                type: 'success',
-                uri: reader.result as string,
-                name: file.name,
-                size: file.size,
-                mimeType: file.type,
-              })
-            }
+      return await Promise.all(
+        fileList.map(async (file) => {
+          const item = await EventFileReader(file)
 
-            reader.readAsDataURL(file as unknown as Blob)
-          })
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          return item as any
+        })
       )
-    )
-  }, [])
+    },
+    []
+  )
 
   return fileReader
 }
