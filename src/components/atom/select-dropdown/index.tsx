3ff543523ad566728e172e065487fd8824719fb7
murilomontino <murilomontinojr@hotmail.com>
2022-04-06 13:07:28 -0300

    [refactor] melhorias no dropdown

--- src/components/atom/select-dropdown/index.tsx
+++ src/components/atom/select-dropdown/index.tsx
@@ -1,38 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import Arrow from './arrow.svg'
 import css from './styles.module.css'
 
-const options = [
-  {
-    value: '0',
-    label: 'Select an option',
-  },
-  {
-    value: '1',
-    label: 'Option 1',
-  },
-  {
-    value: '2',
-    label: 'Option 2',
-  },
-  {
-    value: '3',
-    label: 'Option 3',
-  },
-]
+type Props = {
+  options: Option[]
+  labelDefault?: string
+  onChangeSelect?: (value: string | number) => void
+}
 
 type Option = {
-  value: string
+  value: number | string
   label: string
 }
 
-const SelectDropdown = () => {
-  const [selectState, setSelectState] = useState('0')
+const SelectDropdown = ({
+  labelDefault = 'Selecione uma opção',
+  options = [],
+  onChangeSelect,
+}: Props) => {
+  const [selectState, setSelectState] = useState<string | number>(0)
 
-  useEffect(() => {
-    console.log(selectState)
-  }, [selectState])
+  const optionsList: Option[] = [
+    {
+      value: 0,
+      label: labelDefault,
+    },
+    ...options,
+  ]
 
   const OptionSelected = ({ value, label }: Option) => {
     return (
@@ -41,26 +36,33 @@ const SelectDropdown = () => {
           className={css['select-box__input']}
           type="radio"
           value={value}
-          defaultChecked={selectState === value ? true : false}
+          defaultChecked={selectState == value ? true : false}
         />
         <p className={css['select-box__input-text']}>{label}</p>
       </div>
     )
   }
 
-  const Options = ({ value, label }: Option) => (
-    <li onClick={() => setSelectState(value)}>
-      <label className={css['select-box__option']} aria-hidden>
-        {label}
-      </label>
-    </li>
-  )
+  const Options = ({ value, label }: Option) => {
+    const onClick = () => {
+      onChangeSelect && onChangeSelect(value)
+      setSelectState(value)
+    }
+
+    return (
+      <li onClick={onClick}>
+        <label className={css['select-box__option']} aria-hidden>
+          {label}
+        </label>
+      </li>
+    )
+  }
 
   return (
     <div className={css['select-box']}>
       <div className={css['select-box__current']} tabIndex={2}>
-        {options.map((option, index) => (
-          <OptionSelected key={index} value={option.value} label={option.label} />
+        {optionsList.map((option, index) => (
+          <OptionSelected key={index} value={option.value.toString()} label={option.label} />
         ))}
         <img
           className={css['select-box__icon']}
@@ -70,8 +72,8 @@ const SelectDropdown = () => {
         />
       </div>
       <ul className={css['select-box__list']}>
-        {options.map((option, index) => (
-          <Options key={index} value={option.value} label={option.label} />
+        {optionsList.map((option, index) => (
+          <Options key={index} value={option.value.toString()} label={option.label} />
         ))}
       </ul>
     </div>
