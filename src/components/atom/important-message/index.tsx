fc22ff62809c26836328cb2590537d9671d7fc49
murilomontino <murilomontinojr@hotmail.com>
2022-04-18 16:04:50 -0300

    [component] important message

---
+++ src/components/atom/important-message/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Important } from './styles'
+
+type Props = {
+  message?: string
+}
+
+const ImportantMessage = ({ message = '* Campos Obrigatórios' }: Props) => {
+  return <Important>{message}</Important>
+}
+
+export default ImportantMessage
