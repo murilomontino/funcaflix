3068712da7116d7a5ce25b50e313f4779f023fe3
murilomontino <murilomontinojr@hotmail.com>
2022-04-18 16:04:35 -0300

    [template] frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 // @generated: @expo/next-adapter@2.1.52
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Image } from 'react-native'
 
-import Header from '@/components/organism/header'
+import TemplateFrontEnd from '@/components/templates/frontend'
 
 import background from '@/assets/background-image.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
+    <TemplateFrontEnd>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
       <Image resizeMode="cover" source={{ uri: background }} style={{ width: 200, height: 200 }} />
-    </View>
+    </TemplateFrontEnd>
   )
 }
 
