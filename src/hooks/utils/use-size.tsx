14a2b9276f896b3c6028572c233a780bc5a30e4c
murilomontino <murilomontinojr@hotmail.com>
2022-04-18 16:25:45 -0300

    [update] use-size

--- src/hooks/utils/use-size.tsx
+++ src/hooks/utils/use-size.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Platform } from 'react-native'
 import { useDimensions } from 'react-native-web-hooks'
 
@@ -7,19 +8,30 @@ export const useSize = () => {
   const web = Platform.OS === 'web'
   const { window, screen } = useDimensions()
 
-  const size = web ? window : screen
+  const { width, height } = web ? window : screen
 
-  const SCREEN_SMALLER_THAN_LARGE_SIZE = size.width < theme.CONSTANTS.SCREEN.LARGE
+  const SCREEN_SMALLER_THAN_LARGE_SIZE = useMemo(
+    () => width < theme.CONSTANTS.SCREEN.LARGE,
+    [width]
+  )
 
-  const SCREEN_LARGE_SIZE = size.width >= theme.CONSTANTS.SCREEN.LARGE
+  const size = useMemo(
+    () => ({
+      width,
+      height,
+    }),
+    [width, height]
+  )
 
-  const SCREEN_SMALLER_THAN_MEDIUM_SIZE = size.width < theme.CONSTANTS.SCREEN.MEDIUM
+  const SCREEN_SMALLER_THAN_MEDIUM_SIZE = useMemo(
+    () => width < theme.CONSTANTS.SCREEN.MEDIUM,
+    [width]
+  )
 
   return {
     size,
     web,
     SCREEN_SMALLER_THAN_LARGE_SIZE,
-    SCREEN_LARGE_SIZE,
     SCREEN_SMALLER_THAN_MEDIUM_SIZE,
   }
 }
