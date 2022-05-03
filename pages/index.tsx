227c72981bf341f690e95dd174e253d63a0264a9
murilomontino <murilomontinojr@hotmail.com>
2022-05-03 12:22:56 -0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,25 @@
 // @generated: @expo/next-adapter@2.1.52
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Image } from 'react-native'
+import { Text, View } from 'react-native'
+
+import Image from 'next/image'
 
 import TemplateFrontEnd from '@/components/templates/frontend'
-
-import background from '@/assets/background-image.png'
 
 export default function App() {
   return (
     <TemplateFrontEnd>
       <View style={{ minHeight: 420, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={styles.text}>
+        <Text
+          style={{
+            fontSize: 16,
+            color: '#f2f2f2',
+          }}
+        >
           Welcome to Expo + Next.js + Docker + Git SubModules + Docker-Compose Server 👋
         </Text>
-        <Image
-          resizeMode="cover"
-          source={{ uri: background }}
-          style={{ width: 200, height: 200 }}
-        />
+        <Image src={'/background-image.png'} width={200} height={200} />
       </View>
     </TemplateFrontEnd>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-    color: '#f2f2f2',
-  },
-})
