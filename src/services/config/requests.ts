75e22cb38d36ddb4a731661c1e961d214d2fc2b3
murilomontino <murilomontinojr@hotmail.com>
2022-05-03 11:56:19 -0300

    [feat] arquivo de requests

---
+++ src/services/config/requests.ts
@@ -0,0 +1,9 @@
+const OAuth = {
+  generateOAuthToken: 'generate-oauth-token',
+  verifyOAuthToken: 'verify-oauth-token',
+  refreshOAuthToken: 'refresh-oauth-token',
+}
+
+export default {
+  OAuth,
+}
