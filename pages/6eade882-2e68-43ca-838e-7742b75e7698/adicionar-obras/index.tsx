a8d7886798110fe81068c163415b8885bf2d1819
murilomontino <murilomontinojr@hotmail.com>
2022-05-03 11:53:17 -0300

    [component] Button Social

--- pages/6eade882-2e68-43ca-838e-7742b75e7698/adicionar-obras/index.tsx
+++ pages/6eade882-2e68-43ca-838e-7742b75e7698/adicionar-obras/index.tsx
@@ -17,6 +17,7 @@ export default function AddWorks() {
   const fetch = async () => {
     try {
       const { data } = await api.get<Getter<GettersExhibitions[]>>('exhibitions')
+
       if (data.statusCode === 200) {
         return data.data
       }
---
+++ src/components/molecule/button-social/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import theme from '@/theme'
+import { FontAwesome } from '@expo/vector-icons'
+import Link from 'next/link'
+
+import { ButtonSocialContainer, ButtonSocialText, ContainerIconSocial } from './styles'
+
+type Props = {
+  name:
+    | 'facebook'
+    | 'twitter'
+    | 'instagram'
+    | 'youtube'
+    | 'linkedin'
+    | 'github'
+    | 'pinterest'
+    | 'google'
+  url: string
+}
+
+const ColorButtonSocial = (name) => {
+  switch (name) {
+    case 'facebook':
+      return '#3b5998'
+    case 'twitter':
+      return '#1da1f2'
+    case 'instagram':
+      return '#e1306c'
+    case 'youtube':
+      return '#ff0000'
+    case 'linkedin':
+      return '#0077b5'
+    case 'github':
+      return '#333'
+    case 'pinterest':
+      return '#CB2027'
+    case 'google':
+      return '#DD4B39'
+    default:
+      return '#333'
+  }
+}
+
+const ButtonSocial = ({ name, url }: Props) => {
+  const color = ColorButtonSocial(name)
+
+  return (
+    <Link href={url}>
+      <a target="_blank" style={{ textDecoration: 'none' }}>
+        <ButtonSocialContainer bgColor={color}>
+          <ContainerIconSocial>
+            <FontAwesome name={name} size={24} color={theme.COLORS.TEXT} />
+          </ContainerIconSocial>
+          <ButtonSocialText>Login com {name}</ButtonSocialText>
+        </ButtonSocialContainer>
+      </a>
+    </Link>
+  )
+}
+
+export default ButtonSocial
