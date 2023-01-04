b4a4a8c8411a92ef7d2fec2dee80b580cc33682a
murilomontino <murilomontinojr@hotmail.com>
2023-01-04 15:49:11 -0300

    style image advertisement

--- source/components/molecule/advertisement/index.tsx
+++ source/components/molecule/advertisement/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
 
+import advertisement_01 from './advertisement.jpg'
+
+import { Container } from 'react-bootstrap'
+
 const Advertisement = () => {
     return (
         <React.Fragment>
-            <div className='d-flex flex-row justify-content-center w-100 align-items-center'>
-                <a href='https://funcap.se.gov.br/simposio-do-xlviii-encontro-cultural-de-laranjeiras-2023/' target='_blank' rel='noreferrer'>
-                    <div className='bg-white m-2 p-4 d-flex flex-row justify-content-center w-100 align-items-center card'
-                        style={{
-                            height: '100px',
-                            borderTopWidth: '8px',
-                            borderTopColor: '#9d00eb',
-                            borderTopLeftRadius: '5px',
-                            borderTopRightRadius: '5px',
-                            boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.2)',
+            <section className={'overflow-hidden mb-5 position-relative'}>
+                <Container fluid>
+                    <a href='https://funcap.se.gov.br/simposio-do-xlviii-encontro-cultural-de-laranjeiras-2023/' target='_blank' rel='noreferrer'>
+
+                        <div className='w-100 p-2' style={{
+                            height: '400px',
+                            width: '100%',
+                            zIndex: 999,
                         }}>
-                        <h6 className='text-center font-weight-700 font-size-16 text-capitalize text-dark'>
-                            Inscrição Simpósio do do XLVIII Encontro Cultural de Laranjeiras 2023
-                        </h6>
-                    </div>
-                </a>
-            </div>
+                            <img src={advertisement_01} alt='Encontro Cultural de Laranjeiras' className='w-100 h-100' style={{
+                                objectFit: 'contain',
+                            }} />
+                        </div>
+                    </a>
+                </Container>
+            </section>
         </React.Fragment>
     )
 }
