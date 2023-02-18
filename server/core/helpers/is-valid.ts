924fdfa1425005a2fe94858679754e20b138f5b4
murilomontino <murilomontinojr@hotmail.com>
2023-02-18 15:29:53 -0300

    criação de funções auxiliadores de validação e decoradores

--- server/core/helpers/is-valid.ts
+++ server/core/helpers/is-valid.ts
@@ -1,6 +1,13 @@
 export function isValid(value: any): boolean {
-    return value !== undefined && value !== null && value !== '' && !Number.isNaN(value)
+	return value !== undefined && value !== null && value !== ''
 }
+
+export function isValidID(value: any): boolean {
+	return (
+		isValid(value) && typeof value === 'number' && !isNaN(value) && value > 0
+	)
+}
+
 export function existItemsInArray(value: any): boolean {
-    return Array.isArray(value) && value.length > 0
+	return Array.isArray(value) && value.length > 0
 }
