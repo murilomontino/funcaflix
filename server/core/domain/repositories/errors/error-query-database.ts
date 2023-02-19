8c599e238345057086105d1526cc686cbc5c16c1
murilomontino <murilomontinojr@hotmail.com>
2023-02-19 20:06:49 -0300

    style(types/setters): correção de espaços para tabulação

--- server/core/domain/repositories/errors/error-query-database.ts
+++ server/core/domain/repositories/errors/error-query-database.ts
@@ -3,7 +3,7 @@ import { InterfaceError } from '@/helpers/interface-error'
 export class ErrorQueryDatabase extends Error implements InterfaceError {
 	constructor(
 		message = 'Erro ao executar query no banco de dados, verifique a causa',
-		options
+		options = null
 	) {
 		super(message, options)
 		this.name = 'ErrorQueryDatabase'
