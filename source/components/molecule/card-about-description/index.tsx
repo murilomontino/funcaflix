dcaeb5befa59ea310227d0fc08a53185405583ef
murilomontino <murilomontinojr@hotmail.com>
2023-02-21 13:16:15 -0300

    refactor(card-about-description): melhoria na estilização do código com auxiliar clamp

--- source/components/molecule/card-about-description/index.tsx
+++ source/components/molecule/card-about-description/index.tsx
@@ -1,37 +1,16 @@
-import React, { useMemo } from 'react'
-
-import styled from 'styled-components'
+import React from 'react'
 
 type Props = {
 	about: string
 }
 
-const P = styled.p`
-	-webkit-line-clamp: 6;
-	-webkit-box-orient: vertical;
-	display: -webkit-box;
-	overflow: hidden;
-`
-
 const AboutDescription = ({ about }: Props) => {
-	const elipses = about.length > 1000 ? '...' : ''
-
-	const aboutMemo = useMemo(
-		() =>
-			(about || 'Sem descrição')
-				.replace(/(<([^>]+)>)/gi, '')
-				.slice(0, 1000)
-				.trim()
-				.concat(elipses),
-		[about, elipses]
-	)
-
 	return (
 		<React.Fragment>
-			<div className="container d-flex justify-content-center align-items-center pt-2 ph-2 mt-5">
-				<P className="font-size-16 text-black text-muted text-justify overflow-hidden text-overflow-ellipsis text-indent-2 line-height-2">
-					{aboutMemo}
-				</P>
+			<div className="container d-flex justify-content-center align-items-center pt-2 ph-2 mt-5 ">
+				<p className="font-size-16 text-black text-muted text-justify overflow-hidden text-overflow-ellipsis text-indent-2 line-height-2 clamp-6">
+					{about}
+				</p>
 			</div>
 		</React.Fragment>
 	)
