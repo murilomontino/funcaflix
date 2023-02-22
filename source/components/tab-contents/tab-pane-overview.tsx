f6dc222d7cabdaa9d87a451711eb278229d27d1a
murilomontino <murilomontinojr@hotmail.com>
2023-02-21 22:29:01 -0300

    style: react fragment

--- source/components/tab-contents/tab-pane-overview.tsx
+++ source/components/tab-contents/tab-pane-overview.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Card, CardBody, Col, Row, Table } from 'reactstrap'
 
 type OverviewProps = {
@@ -24,79 +25,81 @@ const TabPaneOverview = ({
 	acting,
 }: OverviewProps) => {
 	return (
-		<Row className={`w-100 justify-content-center align-content-lg-center m-2`}>
-			<Card
-				className="w-30 m-1"
-				style={{
-					maxHeight: '380px',
-				}}
-			>
-				<CardBody>
-					<Col lg={12}>
-						<h5 className="mb-3 text-black text-center">Informações:</h5>
-						<div className="table-responsive">
-							<Table className="table-borderless mb-0">
-								<tbody>
-									<tr>
-										<th className="ps-0" scope="row">
-											Nome Perfil:
-										</th>
-										<td className="text-muted">{name}</td>
-									</tr>
-									<tr>
-										<th className="ps-0" scope="row">
-											Telefone Público:
-										</th>
-										<td className="text-muted">{phone}</td>
-									</tr>
-									<tr>
-										<th className="ps-0" scope="row">
-											Email Público:
-										</th>
-										<td className="text-muted">{email}</td>
-									</tr>
-									<tr>
-										<th className="ps-0" scope="row">
-											Cidade:
-										</th>
-										<td className="text-muted">
-											{city}, {uf}
-										</td>
-									</tr>
-									<tr>
-										<th className="ps-0" scope="row">
-											Segmento:
-										</th>
-										<td className="text-muted">{segment}</td>
-									</tr>
+		<React.Fragment>
+			<Row className="w-100 justify-content-center align-content-lg-center m-2">
+				<Card
+					className="w-30 m-1"
+					style={{
+						maxHeight: '380px',
+					}}
+				>
+					<CardBody>
+						<Col lg={12}>
+							<h5 className="mb-3 text-black text-center">Informações:</h5>
+							<div className="table-responsive">
+								<Table className="table-borderless mb-0">
+									<tbody>
+										<tr>
+											<th className="ps-0" scope="row">
+												Nome Perfil:
+											</th>
+											<td className="text-muted">{name}</td>
+										</tr>
+										<tr>
+											<th className="ps-0" scope="row">
+												Telefone Público:
+											</th>
+											<td className="text-muted">{phone}</td>
+										</tr>
+										<tr>
+											<th className="ps-0" scope="row">
+												Email Público:
+											</th>
+											<td className="text-muted">{email}</td>
+										</tr>
+										<tr>
+											<th className="ps-0" scope="row">
+												Cidade:
+											</th>
+											<td className="text-muted">
+												{city}, {uf}
+											</td>
+										</tr>
+										<tr>
+											<th className="ps-0" scope="row">
+												Segmento:
+											</th>
+											<td className="text-muted">{segment}</td>
+										</tr>
 
-									<tr>
-										<th className="ps-0" scope="row">
-											Atuação:
-										</th>
-										<td className="text-muted">{acting}</td>
-									</tr>
-								</tbody>
-							</Table>
-						</div>
-					</Col>
-				</CardBody>
-			</Card>
-			<Card className="w-50 m-1">
-				<CardBody>
-					<h5 className="mb-3 text-black text-center">Sobre</h5>
-					<p
-						className="text-justify"
-						style={{
-							textJustify: 'inter-word',
-							textIndent: '2em',
-						}}
-					>
-						{about}
-					</p>
-				</CardBody>
-			</Card>
-		</Row>
+										<tr>
+											<th className="ps-0" scope="row">
+												Atuação:
+											</th>
+											<td className="text-muted">{acting}</td>
+										</tr>
+									</tbody>
+								</Table>
+							</div>
+						</Col>
+					</CardBody>
+				</Card>
+				<Card className="w-50 m-1">
+					<CardBody>
+						<h5 className="mb-3 text-black text-center">Sobre</h5>
+						<p
+							className="text-justify"
+							style={{
+								textJustify: 'inter-word',
+								textIndent: '2em',
+							}}
+						>
+							{about}
+						</p>
+					</CardBody>
+				</Card>
+			</Row>
+		</React.Fragment>
 	)
 }
 
