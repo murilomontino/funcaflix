85b14adecd88b9ce11dc23229aae145296e72425
murilomontino <murilomontinojr@hotmail.com>
2023-02-22 14:28:42 -0300

    fix: exit script em caso de sucesso #42

--- server/core/scripts/build-database.ts
+++ server/core/scripts/build-database.ts
@@ -1,9 +1,16 @@
-require('dotenv').config()
-
+import { config } from 'dotenv'
 import { build } from 'mapacultural-database'
+import { exit } from 'process'
 
 async function main() {
-	await build()
+	build()
+		.then(() => {
+			exit()
+		})
+		.catch((error) => {
+			throw error
+		})
 }
 
+config()
 main()
