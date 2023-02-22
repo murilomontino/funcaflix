ebeefaa3f77736f97dab42c73f9986fe61c54d41
murilomontino <murilomontinojr@hotmail.com>
2023-02-22 10:25:32 -0300

    refactor(package): criação de script para teste unitário
    #42

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,24 @@ import path from 'path'
 config()
 
 export default defineConfig({
-  root: __dirname,
-  test: {
-    coverage: {
-      provider: 'istanbul' // or 'c8'
-    },
-    reporters: ['html', 'default'],
-    testTimeout: 5000000,
-    deps: {
-      inline: true,
-    },
-    exclude: ['**/node_modules/**', '**/dist/**', "**/.docker/**"],
-  },
-  resolve: {
-    alias: {
-      '@/database': path.resolve(__dirname, 'backend', 'database'),
-      '@/types': path.resolve(__dirname, 'types'),
-      "@/__mocks__": path.resolve(__dirname, '__mocks__'),
-      '@': path.resolve(__dirname, 'server', 'core'),
-    },
-  },
+	root: __dirname,
+	test: {
+		coverage: {
+			provider: 'istanbul', // or 'c8'
+		},
+		reporters: ['html', 'default'],
+		testTimeout: 5000000,
+		deps: {
+			inline: true,
+		},
+		exclude: ['**/node_modules/**', '**/dist/**', '**/.docker/**'],
+	},
+	resolve: {
+		alias: {
+			'@/database': path.resolve(__dirname, 'backend', 'database'),
+			'@/types': path.resolve(__dirname, 'types'),
+			'@/__mocks__': path.resolve(__dirname, '__mocks__'),
+			'@': path.resolve(__dirname, 'server', 'core'),
+		},
+	},
 })
