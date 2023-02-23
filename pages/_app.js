cbf7cb7e9b74927154f6bedb80ab5eef1b012d9d
murilomontino <murilomontinojr@hotmail.com>
2023-02-23 10:51:57 -0300

    fix: correções de importação que geravam quebra na build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
-import RootContext from '@/context/Root'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import 'react-loading-skeleton/dist/skeleton.css'
@@ -10,25 +8,18 @@ import 'swiper/components/pagination/pagination.min.css'
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 
-import TemplateFrontEnd from '@/components/templates/frontend'
 import 'raf/polyfill'
 import 'setimmediate'
 import '../styles/_styles.scss'
 
-const queryClient = new QueryClient()
-// This default export is required in a new `pages/_app.js` file.
-const MyApp = ({ Component, pageProps }) => {
+import AppCustom from '@/App'
+
+const App = ({ Component, pageProps }) => {
 	return (
 		<React.Fragment>
-			<QueryClientProvider client={queryClient}>
-				<RootContext>
-					<TemplateFrontEnd>
-						<Component {...pageProps} />
-					</TemplateFrontEnd>
-				</RootContext>
-			</QueryClientProvider>
+			<AppCustom Component={Component} pageProps={pageProps} />
 		</React.Fragment>
 	)
 }
 
-export default MyApp
+export default App
